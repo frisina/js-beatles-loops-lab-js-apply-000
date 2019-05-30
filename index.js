565216cf2acad0d93b70f143ec16090b9ad8d499
Marco Frisina <marco@frisina.xyz>
2019-05-30 10:43:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var i = 0;
-  while (i <= facts.length) {
+  while (i < facts.length) {
     return facts[i] + "!!!";
     i++;
   }
