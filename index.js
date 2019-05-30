cd460435ab62911bccb8b067467503988e29383c
Marco Frisina <marco@frisina.xyz>
2019-05-30 10:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // add solution here
+
+
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = [];
   for (i = 0; i < musicians.length; i++)
   {
     emptyArray += musicians[i] + " plays " + instruments[i];
   }
+  return emptyArray;
 }
